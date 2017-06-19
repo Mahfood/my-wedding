0f791643dc65312fa46a6aeb996864f65c2c2ffb
mPanasiewicz <grunek1@gmail.com>
2017-06-19 23:57:28 +0200

    Remove extra div in 80 row

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,25 @@
 class HomeController < ApplicationController
-  before_action :allow_page_caching
+  before_action :resourceful_cache_headers
+  before_action :resourceless_cache_headers
+  before_action :skip_session
 
-  def index
+  def skip_session
+    request.session_options[:skip] = true
   end
 
-  private
-
-  def allow_page_caching
-    expires_in(5.minutes) unless Rails.env.development?
+  def resourceless_cache_headers
+    fresh_when(nil, template: "#{controller_path}/#{action_name}")
   end
+
+  def resourceful_cache_headers
+    fresh_when(@resource)
+  end
+
+  skip_before_action :verify_authenticity_token, only: [:create]
+
+  # also may be better to remove csrf token from layout of cached pages
+  # don't forget than any kind of set cookies will pass cache,
+  # be aware that some gems can do this for you, like, for instance, rack-mini-profiler,
+  # It's reasonable NOT to interfere Set-Cookie on NGINX layer,
+  # because Nginx can't be sure intended or not this behaviour
 end
